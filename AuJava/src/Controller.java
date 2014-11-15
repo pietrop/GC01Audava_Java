b7fd8dc4d28431499929a2f0b93b71f814baaa1a
Pietro Passarelli <pietro.passarelli@gmail.com>
2014-11-15 21:48:32 +0000

    changed album info panel, added picture, and changed textLabel  to textArea, as will make it easyer to edit just by changing editable to true

--- AuJava/src/Controller.java
+++ AuJava/src/Controller.java
@@ -6,7 +6,7 @@ public class Controller {
 	public static void main(String[] args) {
 		AlbumPage albumPage = new AlbumPage();// create JFrame window(?)
 		albumPage.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		albumPage.setSize( 600, 300 );
+		albumPage.setSize( 700, 500 );
 		albumPage.setVisible( true ); 
 		albumPage.setResizable(true);
 	}
--- AuJava/src/JPanels/AlbumInfoPanel.java
+++ AuJava/src/JPanels/AlbumInfoPanel.java
@@ -1,9 +1,13 @@
 package JPanels;
 import java.awt.GridBagLayout;
+import java.awt.TextArea;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JProgressBar;
+
 
 
 public class AlbumInfoPanel extends JPanel{
@@ -16,14 +20,31 @@ public class AlbumInfoPanel extends JPanel{
 	
 	private void populatePanel(String title, String description){
 		
-		JLabel albumTitleLabel = new JLabel(title);
-		albumTitleLabel.setBounds(100, 6, 73, 10);
-		JLabel albumDescriptionLabel = new JLabel(description);
-		albumDescriptionLabel.setBounds(10, 20, 279, 171);
+//		JLabel albumTitleLabel = new JLabel(title);
+		TextArea albumTitleLabel = new TextArea(title,1,1, TextArea.SCROLLBARS_NONE);
+		albumTitleLabel.setBounds(107, 10, 256, 29);
+		albumTitleLabel.setEditable(false);
+//		JLabel albumDescriptionLabel = new JLabel(description);
+		TextArea albumDescriptionLabel = new TextArea(description,1,10,TextArea.SCROLLBARS_NONE);
+		albumDescriptionLabel.setEditable(false);
+		albumDescriptionLabel.setBounds(20, 80, 343, 196);
+		
 		setLayout(null);
 		add(albumTitleLabel);
 		add(albumDescriptionLabel);
+		//album title
+		JLabel lblAlbumTitle = new JLabel("Album Title");
+		lblAlbumTitle.setBounds(20, 23, 112, 16);
+		add(lblAlbumTitle);
+		
+		//album description
+		JLabel albulmDescription = new JLabel("Album Description");
+		albulmDescription.setBounds(20, 58, 131, 16);
+		add(albulmDescription);
+		//album image
+		ImageIcon  albumPictureIcon = new ImageIcon("duke-java.png");
+		JLabel alblAlbumImage = new JLabel(albumPictureIcon);
+		alblAlbumImage.setBounds(388, 11, 289, 188);
+		add(alblAlbumImage);
 	}
-	
-	
 }
